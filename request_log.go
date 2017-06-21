9f7220ac078ed330351cf61a2b6be25e19a537a7
Slavik Shynkarenko <shinkarenko.vi@gmail.com>
2017-06-21 20:38:44 +0300

    Request log with ability to add and count hits.

---
+++ request_log.go
@@ -0,0 +1,45 @@
+/**
+ * @Author: Viacheslav Shynkarenko <Slavik>
+ * @Date:   2017-06-21T20:15:17+03:00
+ * @Email:  shinkarenko.vi@gmail.com
+ * @Last modified by:   Slavik
+ * @Last modified time: 2017-06-21T20:36:34+03:00
+ * @Copyright: Viacheslav Shynkarenko. All Rights Reserved.
+ */
+
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+// RequestLog stores request timestamps for the last 60 seconds.
+type RequestLog struct {
+	// Collection of timestamps
+	log []time.Time
+
+	// Mutex for synchronization between goroutines
+	mutex sync.Mutex
+}
+
+// NewRequestLog creates new instance of the RequestLog.
+func NewRequestLog() *RequestLog {
+	return &RequestLog{}
+}
+
+// Hit adds current time to the log.
+func (requestLog *RequestLog) Hit() {
+	requestLog.log = append(requestLog.log, time.Now())
+}
+
+// MinuteHitsTotal returns the amount of hits occured in the past 60 sec.
+func (requestLog *RequestLog) MinuteHitsTotal() int {
+	for idx, timestamp := range requestLog.log {
+		if time.Since(timestamp).Seconds() > 60 {
+			requestLog.log = requestLog.log[idx:]
+			return len(requestLog.log)
+		}
+	}
+	return len(requestLog.log)
+}
