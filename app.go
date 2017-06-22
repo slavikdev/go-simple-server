d53f2d475ed5b3e3a48f82b1c69642229668ef22
Slavik Shynkarenko <shinkarenko.vi@gmail.com>
2017-06-22 03:09:03 +0300

    Catch interruptions and save state before exit.

--- app.go
+++ app.go
@@ -3,16 +3,26 @@
  * @Date:   2017-06-21T20:10:12+03:00
  * @Email:  shinkarenko.vi@gmail.com
  * @Last modified by:   Slavik
- * @Last modified time: 2017-06-22T02:39:37+03:00
+ * @Last modified time: 2017-06-22T03:06:07+03:00
  * @Copyright: Viacheslav Shynkarenko. All Rights Reserved.
  */
 
 package main
 
+import "os"
+import "os/signal"
+import "syscall"
+
 func main() {
 	stateFile := NewStateFile()
 	rqlog := NewRequestLogFromState(stateFile.Load())
 	server := NewServer(rqlog)
-	defer stateFile.Save(NewState(rqlog))
+	signalsChannel := make(chan os.Signal, 1)
+	signal.Notify(signalsChannel, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-signalsChannel
+		stateFile.Save(NewState(rqlog))
+		os.Exit(0)
+	}()
 	server.Start()
 }
