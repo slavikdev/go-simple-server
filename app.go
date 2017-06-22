d35a5ce6479a40b5555c2266298d0496864f55e0
Slavik Shynkarenko <shinkarenko.vi@gmail.com>
2017-06-22 03:10:30 +0300

    Imports refactoring.

--- app.go
+++ app.go
@@ -3,15 +3,17 @@
  * @Date:   2017-06-21T20:10:12+03:00
  * @Email:  shinkarenko.vi@gmail.com
  * @Last modified by:   Slavik
- * @Last modified time: 2017-06-22T03:06:07+03:00
+ * @Last modified time: 2017-06-22T03:09:37+03:00
  * @Copyright: Viacheslav Shynkarenko. All Rights Reserved.
  */
 
 package main
 
-import "os"
-import "os/signal"
-import "syscall"
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	stateFile := NewStateFile()
